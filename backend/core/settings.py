d9c0e72397552a594df448a021c1f14e9160cfe4
alex <alexxxd23@gmail.com>
2022-09-15 12:26:54 -0300

    fix error static

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -137,9 +137,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
 
 MEDIA_URL = '/media/'
 
