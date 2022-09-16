a7e09c60cad2f1f7243c2df9a666912621a3c0fa
alex <alexxxd23@gmail.com>
2022-09-16 03:01:07 -0300

    update

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from .views import HomeRest
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('applications/', include('applications.food.urls')),
+    path('', HomeRest.as_view(), name="homeRest"),
+    path('', include('applications.food.urls')),
     path('', include('applications.user.api.urls')),
 ]
 if settings.DEBUG:
