5c752c9e6b70733435cd42e3c9584c659aeb6b42
alex <alexxxd23@gmail.com>
2022-09-17 22:22:55 -0300

    asd

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -11,7 +11,11 @@ urlpatterns = [
     path('', include('applications.food.urls')),
     path('', include('applications.user.api.urls')),
 <<<<<<< HEAD
+<<<<<<< HEAD
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 =======
 ] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 >>>>>>> beda105 (fix...)
+=======
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+>>>>>>> 3c0f584 (asd)
