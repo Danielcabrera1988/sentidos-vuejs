293cc401826aae4ed207e431d3397cb4918326bc
Daniel Alejandro Cabrera <cabrera_daniel0104@hotmail.com>
2022-09-18 22:33:50 -0300

    change url api

--- src/Ax-Api.js
+++ src/Ax-Api.js
@@ -1,11 +1,7 @@
 import axios from 'axios'
 
 const getAPI = axios.create({
-<<<<<<< HEAD
-    baseURL: 'http://alexlopez.pythonanywhere.com'
-=======
     baseURL: 'https://binarysystem.pythonanywhere.com/'
->>>>>>> dani
 })
 
 export { getAPI }
